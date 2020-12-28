f04e38f5eb23b9ef06d952ff538b560909d157b6
heojungeun <heocube@naver.com>
2020-12-28 22:50:29 +0900

    메모 보기 화면 xml 편집

--- source/app/src/main/java/com/example/escape_two/ui/page_View1/ItemViewActivity.kt
+++ source/app/src/main/java/com/example/escape_two/ui/page_View1/ItemViewActivity.kt
@@ -1,12 +1,13 @@
-package com.example.escape_two.ui
+package com.example.escape_two.ui.page_View1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.escape_two.R
 
 class ItemViewActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_itemview)
     }
 }
